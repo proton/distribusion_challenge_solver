bbe4a37e6157c14f956bfb46f586052cdb82e93f
Peter Savichev (proton) <psavichev@gmail.com>
2017-10-30 21:38:19 +0300

    updated specs

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,7 +3,11 @@ require 'solver'
 describe DistributionChallenge::Solver do
   describe '#call' do
     it 'returns password' do
-      expect(described_class.call).to eq(true)
+      result = { :'(•̀_•́ )' => 'Targeting... almost there. Lock! I got him!',
+                 :'(⌐■_■)' => 'Now, Tank now!',
+                 :'--------' => '--------',
+                 :'ヽ(´‿｀)ノ'  => 'Welcome to the real world, Neo.' }
+      expect(described_class.call).to eq(result)
     end
   end
 end
