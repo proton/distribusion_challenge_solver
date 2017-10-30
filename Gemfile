5340bf5cccccac2556455f83c4000c4168fe1b21
Peter Savichev (proton) <psavichev@gmail.com>
2017-10-30 20:56:41 +0300

    using vcr

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source 'https://rubygems.org'
 gem 'unirest'
 gem 'rubyzip', '>= 1.0.0'
 
+gem 'webmock'
 gem 'rspec'
+gem 'vcr'
--- spec/password_loader_spec.rb
+++ spec/password_loader_spec.rb
@@ -3,7 +3,9 @@ require 'password_loader'
 describe DistributionChallenge::PasswordLoader do
   describe '#call' do
     it 'returns password' do
-      expect(described_class.call).to eq('Kans4s-i$-g01ng-by3-bye')
+      VCR.use_cassette 'load_password' do
+        expect(described_class.call).to eq('Kans4s-i$-g01ng-by3-bye')
+      end
     end
   end
 end
