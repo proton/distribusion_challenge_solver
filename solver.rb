9c5381fa77cd040370d7b28749b844dcdffd667f
Peter Savichev (proton) <psavichev@gmail.com>
2017-10-30 19:45:45 +0300

    universal entities loader

--- solver.rb
+++ solver.rb
@@ -10,18 +10,36 @@ def get_password
   data['pills']['red']['passphrase']
 end
 
+def parse_csv_entities(entry)
+  content = entry.get_input_stream.read
+  CSV.parse(content, quote_char: '"', col_sep: ', ', headers: :first_row).map(&:to_hash)
+end
+
+def parse_json_entities(entry)
+  content = entry.get_input_stream.read
+  h = JSON.parse(content)
+  main_key = h.keys.first
+  h[main_key]
+end
+
 def extract_entities(data)
   entity_regex = /\w+\/(?<name>\w+)\.(?<extention>\w+)$/
+  result = {}
+  # loading
   Zip::File.open_buffer(data) do |zip|
     zip.each do |entry|
       m = entry.name.match entity_regex
       next unless m
-      p m
+      entity_name = m[:name]
+      result[entity_name] = case m[:extention]
+      when 'csv'
+        parse_csv_entities(entry)
+      when 'json'
+        parse_json_entities(entry)
+      end
     end
-    # content = routes_entry.get_input_stream.read
-    # arr = CSV.parse(content, quote_char: '"', col_sep: ', ', headers: :first_row)
-    # p arr[0]
   end
+  result
 end
 
 password = get_password
