76841c131e1e1e2738a6d92033242b7e2415812c
Peter Savichev (proton) <psavichev@gmail.com>
2017-10-30 18:58:51 +0300

    started moving to functions

--- solver.rb
+++ solver.rb
@@ -2,27 +2,52 @@ require 'unirest'
 require 'zip'
 require 'csv'
 
-# get pills
-
-initial_url = 'https://challenge.distribusion.com/the_one'
-headers = { 'Accept' => 'application/json' }
-response = Unirest.get initial_url, headers: headers
-data = response.body
-password = data['pills']['red']['passphrase']
-
-p password
-
-# get sentinels
-
-routes_url = 'https://challenge.distribusion.com/the_one/routes'
-
-source = 'sentinels'
-parameters = { source: source, passphrase: password }
-response = Unirest.get routes_url, parameters: parameters
-
-Zip::File.open_buffer(response.body) do |zip|
-  routes_entry = zip.detect { |entry| entry.name =~ /^#{source}.*\/routes.*\.csv$/ }
-  content = routes_entry.get_input_stream.read
-  arr = CSV.parse(content, quote_char: '"', col_sep: ', ', headers: :first_row)
-  p arr[0]
+def get_password
+  url = 'https://challenge.distribusion.com/the_one'
+  headers = { 'Accept' => 'application/json' }
+  response = Unirest.get url, headers: headers
+  data = response.body
+  data['pills']['red']['passphrase']
 end
+
+password = get_password
+
+all_routes = []
+
+sources = %w(sentinels sniffers loopholes)
+sources.each do |source|
+  url = 'https://challenge.distribusion.com/the_one/routes'
+  parameters = { source: source, passphrase: password }
+  response = Unirest.get url, parameters: parameters
+
+  entity_regex = /^#{source}.*\/\w+.*$/
+  Zip::File.open_buffer(response.body) do |zip|
+    entries = zip.select { |entry| entry.name =~ entity_regex }
+    puts entries
+    # routes_entry = zip.detect { |entry| entry.name =~ /^#{source}.*\/routes.*\.csv$/ }
+    # content = routes_entry.get_input_stream.read
+    # arr = CSV.parse(content, quote_char: '"', col_sep: ', ', headers: :first_row)
+    # p arr[0]
+  end
+end
+
+# Zip::File.open_buffer(response.body) do |zip|
+#   routes_entry = zip.detect { |entry| entry.name =~ /^#{source}.*\/routes.*\.csv$/ }
+#   content = routes_entry.get_input_stream.read
+#   arr = CSV.parse(content, quote_char: '"', col_sep: ', ', headers: :first_row)
+#   p arr[0]
+# end
+#
+# # get sniffers
+#
+# source = 'sniffers'
+# parameters = { source: source, passphrase: password }
+# response = Unirest.get routes_url, parameters: parameters
+#
+# Zip::File.open_buffer(response.body) do |zip|
+#   puts zip.to_a
+#   # routes_entry = zip.detect { |entry| entry.name =~ /^#{source}.*\/routes.*\.csv$/ }
+#   # content = routes_entry.get_input_stream.read
+#   # arr = CSV.parse(content, quote_char: '"', col_sep: ', ', headers: :first_row)
+#   # p arr[0]
+# end
